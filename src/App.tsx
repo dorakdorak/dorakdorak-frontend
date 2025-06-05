d7a96c658e66e9c23a89b702e5c48b536d2d0339
hyeonzi423 <chik1y@kookmin.ac.kr>
2025-06-06 00:50:16 +0900

    [FEAT] 라우터 설정 (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import "@/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "@/assets/font/font.css";
+import "@/App.css";
 import Header from "@/components/common/Header";
-import Footer from "./components/common/Footer";
+import Footer from "@/components/common/Footer";
+import Main from "@/pages/Main";
+import Menu from "@/pages/Menu";
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header isLoggedIn={false} />
-      <main className="main-content">
-        <section className="section">
-          <div style={{ flex: 1, background: "#eee", height: "402px" }}>본문</div>
-        </section>
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header isLoggedIn={false} />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/menu" element={<Menu />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
+
 export default App;
