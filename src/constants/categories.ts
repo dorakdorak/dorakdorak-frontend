53f706b7f41a32557620172671e2ced88b681bee
hyeonzi423 <chik1y@kookmin.ac.kr>
2025-06-06 08:00:26 +0900

    [FIX] 폴더 구조 변경으로 인한 import 수정 (#5)

---
+++ src/constants/categories.ts
@@ -0,0 +1,17 @@
+import all from "@/assets/images/category/all.png";
+import heart from "@/assets/images/category/heart.png";
+import weight from "@/assets/images/category/weight.png";
+import star from "@/assets/images/category/star.png";
+import dumbbell from "@/assets/images/category/dumbbell.png";
+import injection from "@/assets/images/category/injection.png";
+import wallet from "@/assets/images/category/wallet.png";
+
+export const categories = [
+  { label: "전체", icon: all },
+  { label: "고혈압 식단", icon: heart },
+  { label: "칼로리 식단", icon: weight },
+  { label: "스페셜 식단", icon: star },
+  { label: "단백질 식단", icon: dumbbell },
+  { label: "당뇨 식단", icon: injection },
+  { label: "가성비 식단", icon: wallet },
+];
