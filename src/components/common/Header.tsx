393b65bf83b51e93612a363145e4ad4c5cba0d31
hyeonzi423 <chik1y@kookmin.ac.kr>
2025-06-07 22:39:06 +0900

    [REFACTOR] 헤더 컴포넌트 CSS 네임 수정 (#13)

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -9,20 +9,20 @@ function Header({ isLoggedIn }: HeaderProps) {
   return (
     <header className="header">
       <div className="header-container">
-        <div className="logo">
+        <div className="header-logo">
           <Link to="/" style={{ display: "flex", alignItems: "center", gap: "4px" }}>
             <img src="/images/logo2.png" alt="도락도락 로고" width={24} height={24} />
             <strong>도락도락</strong>
           </Link>
         </div>
-        <nav className="nav-menu">
+        <nav className="header-nav-menu">
           <Link to="/menu">도시락 조회</Link>
           <Link to="/group-order">공구 주문</Link>
           <Link to="/custom-dosirak">나만의 도시락 만들기</Link>
           <Link to="/custom-ranking">커스텀 랭킹</Link>
           <Link to="/zero-waste">제로 웨이스트 랭킹</Link>
         </nav>
-        <div className="user-menu">
+        <div className="header-user-menu">
           {isLoggedIn ? (
             <>
               <Link to="/mypage">마이페이지</Link>
