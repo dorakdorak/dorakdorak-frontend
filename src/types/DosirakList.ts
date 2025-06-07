56c9a4273dcf5179b5fdc9356eeb77e751eab44f
hyeonzi423 <chik1y@kookmin.ac.kr>
2025-06-07 12:56:17 +0900

    [FEAT] 도시락 리스트 API 임시 구현 (#10)

---
+++ src/types/DosirakList.ts
@@ -0,0 +1,18 @@
+import { FilterType } from "@/constants/categories";
+import { SortType } from "@/constants/sortOptions";
+import { StorageType } from "@/types/DosirakDetail";
+
+export interface DosirakRequest {
+  dosirakId?: number;
+  filterType: FilterType;
+  sortType: SortType;
+}
+
+export interface DosirakItem {
+  dosirakId: number;
+  name: string;
+  price: number;
+  salesPercentage: number;
+  storageType: StorageType;
+  imageUrl: string;
+}
