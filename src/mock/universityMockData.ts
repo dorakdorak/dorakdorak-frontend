a9bb937a62a20696953a33fb1e7657f752d988c5
yss0512 <dbtntkd456@naver.com>
2025-06-08 13:34:33 +0900

    [FEAT] 회원가입 페이지  구현
    - 알러지, 대학교 목업 데이터 생성(추후 API )
    - 카카오 우편번호 API
    - React Query로 API 초기 데이터 캐싱 방법

---
+++ src/mock/universityMockData.ts
@@ -0,0 +1,19 @@
+export interface University {
+  id: number;
+  name: string;
+}
+
+export const universityData: University[] = [
+  { id: 1, name: '서울대학교' },
+  { id: 2, name: '연세대학교' },
+  { id: 3, name: '고려대학교' },
+  { id: 4, name: '서강대학교' },
+  { id: 5, name: '성균관대학교' },
+  { id: 6, name: '한양대학교' },
+  { id: 7, name: '이화여자대학교' },
+  { id: 8, name: '중앙대학교' },
+  { id: 9, name: '경희희대학교' },
+  { id: 10, name: '서울시립대학교' },
+];
+
+export default universityData;
