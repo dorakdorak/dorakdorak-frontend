6870f7ece33d4052cc8ce6d6e3ee654d6daafb86
김현지 <chik1y@kookmin.ac.kr>
2025-06-10 15:06:41 +0900

    [FEAT] 질문 컴포넌트 구현 (#22)

---
+++ src/components/common/select/SelectBox.tsx
@@ -0,0 +1,31 @@
+import SelectableItem from "@/components/common/select/SelectableItem";
+import styles from "@/css/common/SelectableItem.module.css";
+
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+type Props = {
+  title: string;
+  options: Option[];
+  selectedValue: string;
+  onChange: (value: string) => void;
+};
+
+export default function SelectBox({ title, options, selectedValue, onChange }: Props) {
+  return (
+      <div className={styles.container}>
+      <h3>{title}</h3>
+      {options.map((option) => (
+        <SelectableItem
+          key={option.value}
+          label={option.label}
+          selected={selectedValue === option.value}
+          onClick={() => onChange(option.value)}
+        />
+      ))}
+    </div>
+  );
+}
