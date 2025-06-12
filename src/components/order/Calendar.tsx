7658fe99bfe120603e6e96dea8265b0b5ba02e41
김현지 <chik1y@kookmin.ac.kr>
2025-06-12 18:03:58 +0900

    [FEAT] 달력 컴포넌트 구현 (#30)

---
+++ src/components/order/Calendar.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { Calendar } from "react-calendar";
+import styles from "@/css/order/CalendarComponent.module.css";
+
+interface CalendarComponentProps {
+  onDateChange: (selectedDate: string) => void;
+}
+
+export default function CalendarComponent({
+  onDateChange,
+}: CalendarComponentProps) {
+  const [date, setDate] = React.useState<Date | null>(null);
+
+  const formatDateToString = (date: Date) => {
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
+      2,
+      "0"
+    )}-${String(date.getDate()).padStart(2, "0")}`;
+  };
+
+  React.useEffect(() => {
+    const today = new Date();
+    setDate(today);
+    onDateChange(formatDateToString(today));
+  }, [onDateChange]);
+
+  if (!date) return null;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.calendarWrapper}>
+        <Calendar
+          calendarType="gregory"
+          onChange={(value) => {
+            if (value instanceof Date) {
+              setDate(value);
+              onDateChange(formatDateToString(value));
+            }
+          }}
+          value={date}
+          locale="ko-KR"
+          navigationLabel={({ date }) => (
+            <div className={styles.navigationLabel}>
+              <span className={styles.navigationLabelText}>
+                {date.toLocaleDateString("ko", {
+                  year: "numeric",
+                  month: "long",
+                })}
+              </span>
+            </div>
+          )}
+          prevLabel={<span>&lt;</span>}
+          nextLabel={<span>&gt;</span>}
+          formatDay={(locale, date) => date.getDate().toString()}
+          formatShortWeekday={(locale, date) =>
+            date.toLocaleDateString("ko", { weekday: "short" })
+          }
+          showNeighboringMonth={false}
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/GroupOrder.tsx
+++ src/pages/GroupOrder.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import CalendarComponent from "@/components/order/Calendar";
+
 function GroupOrder() {
+  const [selectedDate, setSelectedDate] = useState<string>("");
+  console.log(selectedDate);
+
   return (
     <div>
       <h1>공구주문</h1>
+      <CalendarComponent onDateChange={setSelectedDate} />
     </div>
   );
 }
