00140245c1f6044cbc71cf4c22a37d02a5843086
hyeonzi423 <chik1y@kookmin.ac.kr>
2025-06-13 23:06:11 +0900

    [FEAT] 주문 리스트 컴포넌트 구현 (#36)

---
+++ src/components/adminManagement/AdminOrderList.tsx
@@ -0,0 +1,42 @@
+import styles from "@/css/admin/Manage.module.css";
+import { AdminOrderResponse } from "@/types/AdminManagement";
+import OrderStatusDropdown from "@/components/adminManagement/OrderStatusDropdown";
+import SectionHeader from "../common/SectionHeader";
+
+interface Props {
+  orders: AdminOrderResponse[];
+  onStatusChange: (orderId: number, newStatus: AdminOrderResponse["orderStatus"]) => void;
+}
+
+export default function AdminOrderList({ orders, onStatusChange }: Props) {
+  return (
+    <div className={styles.wrapper}>
+      <SectionHeader title="주문 관리"></SectionHeader>
+
+      <div className={`${styles.table}`}>
+        <div className={`${styles.row} ${styles.headerRow}`}>
+          <div>주문코드</div>
+          <div>유형</div>
+          <div>가격</div>
+          <div>도착일</div>
+          <div>상태</div>
+        </div>
+
+        {orders.map((order) => (
+          <div className={styles.row} key={order.orderId}>
+            <div>{order.orderCode}</div>
+            <div>{order.type === "SINGLE" ? "일반" : "공구"}</div>
+            <div>{order.price.toLocaleString()}원</div>
+            <div>{order.arrivedAt}</div>
+            <div>
+              <OrderStatusDropdown
+                currentStatus={order.orderStatus}
+                onChange={(newStatus) => onStatusChange(order.orderId, newStatus)}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
