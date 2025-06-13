917e4d9e759e507721e77bdb0e8101ae28060e41
price126 <hwjun12@gmail.com>
2025-06-13 16:02:25 +0900

    [FIX] localhost 변경

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -40,7 +40,7 @@ axiosInstance.interceptors.response.use(
 
     if ((statusCode === 419 || statusCode === 401) && !isLoginRequest) {
       try {
-        const startUrl = 'http://localhost:8080';
+        const startUrl = 'https://dorakdorak.store/';
         const refreshResponse = await axios.post(
           startUrl + '/api/auth/reissue',
           {},
