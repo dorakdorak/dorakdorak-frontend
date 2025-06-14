c606100809603abde5b51d10338c7177fab60545
hyeonzi423 <chik1y@kookmin.ac.kr>
2025-06-14 17:10:12 +0900

    [FEAT] 어드민 인기 도시락 목업 데이터 생성 (#38)

---
+++ src/mock/AdminPopularDosiraks.ts
@@ -0,0 +1,67 @@
+import thumbnail from "@/assets/images/mock/meal2.jpg";
+
+export const mockPopularDosirak = [
+  {
+    rank: 1,
+    dosirakId: 101,
+    name: "든든한 치킨도시락",
+    imageUrl: thumbnail,
+    count: 132,
+  },
+  {
+    rank: 2,
+    dosirakId: 102,
+    name: "소불고기 정찬 도시락",
+    imageUrl: thumbnail,
+    count: 121,
+  },
+  {
+    rank: 3,
+    dosirakId: 103,
+    name: "매콤제육 도시락",
+    imageUrl: thumbnail,
+    count: 115,
+  },
+  {
+    rank: 4,
+    dosirakId: 104,
+    name: "두부 스테이크 도시락",
+    imageUrl: thumbnail,
+    count: 98,
+  },
+  {
+    rank: 5,
+    dosirakId: 105,
+    name: "가성비 혜자 도시락",
+    imageUrl: thumbnail,
+    count: 91,
+  },
+  {
+    rank: 6,
+    dosirakId: 106,
+    name: "새우튀김 도시락",
+    imageUrl: thumbnail,
+    count: 85,
+  },
+  {
+    rank: 7,
+    dosirakId: 107,
+    name: "고기 듬뿍 삼겹도시락",
+    imageUrl: thumbnail,
+    count: 78,
+  },
+  {
+    rank: 8,
+    dosirakId: 108,
+    name: "치즈돈까스 도시락",
+    imageUrl: thumbnail,
+    count: 69,
+  },
+  {
+    rank: 9,
+    dosirakId: 109,
+    name: "닭가슴살 건강 도시락",
+    imageUrl: thumbnail,
+    count: 62,
+  },
+];
