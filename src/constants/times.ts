9f23fc306d96c372251d65ef4151b03e2783e453
김현지 <chik1y@kookmin.ac.kr>
2025-06-16 16:59:59 +0900

    [FEAT] 공동 주문 현황 API 연결 (#49)

--- src/constants/times.ts
+++ src/constants/times.ts
@@ -4,9 +4,9 @@ export interface TimeOptionItem {
 }
 
 export const TIME_OPTIONS: TimeOptionItem[] = [
-  { label: "오전 10시", value: "10:00" },
-  { label: "오전 11시", value: "11:00" },
-  { label: "오후 12시", value: "12:00" },
-  { label: "오후 1시", value: "13:00" },
-  { label: "오후 2시", value: "14:00" },
+  { label: "오전 9시", value: "9" },
+  { label: "오전 10시", value: "10" },
+  { label: "오전 11시", value: "11" },
+  { label: "오후 12시", value: "12" },
+  { label: "오후 1시", value: "13" },
 ];
--- src/pages/GroupOrder.tsx
+++ src/pages/GroupOrder.tsx
@@ -5,17 +5,39 @@ import SelectBox from "@/components/common/select/SelectBox";
 import { TIME_OPTIONS } from "@/constants/times";
 import styles from "@/css/order/GroupOrder.module.css";
 import DosirakOrderSection from "@/components/order/DosirakOrderSection";
+import { fetchGroupOrders } from "@/api/GroupOrder";
+import GroupOrderItem from "@/types/GroupOrder";
 
 function GroupOrder() {
-  const [selectedDate, setSelectedDate] = useState<string>("");
-  const [selectedTime, setSelectedTime] = useState<string>("");
-  console.log(selectedDate);
+  const getToday = () => new Date().toISOString().split("T")[0];
+
+  const [selectedDate, setSelectedDate] = useState<string>(getToday());
+  const [selectedTime, setSelectedTime] = useState<string>("9");
+  const [orderList, setOrderList] = useState<GroupOrderItem[]>([]);
+
   useEffect(() => {
     document.body.classList.add("bg-custom");
     return () => {
       document.body.classList.remove("bg-custom");
     };
   }, []);
+
+  useEffect(() => {
+    if (selectedDate && selectedTime) {
+      const request = {
+        arriveAt: selectedDate,
+        arriveTime: selectedTime,
+      };
+
+      fetchGroupOrders(request)
+        .then(setOrderList)
+        .catch((err) => {
+          console.error("공동 주문 목록 불러오기 실패", err);
+          setOrderList([]);
+        });
+    }
+  }, [selectedDate, selectedTime]);
+
   return (
     <div className={styles.wrapper}>
       <SectionHeader title="공동 주문" />
@@ -33,7 +55,7 @@ function GroupOrder() {
         </div>
       </div>
       <div className={styles.orderSection}>
-        <DosirakOrderSection />
+        <DosirakOrderSection orderList={orderList} />
       </div>
     </div>
   );
