715139617b49e5fd389ab5d50d682d9358e0b847
hyeonzi423 <chik1y@kookmin.ac.kr>
2025-06-17 22:25:14 +0900

    [FEAT] 어드민 주문 상태 조회 및 상태 변경 API 연결 (#57)

---
+++ src/api/AdminApi.ts
@@ -0,0 +1,32 @@
+import axiosInstance from "@/api/axiosInstance";
+import { AdminOrderResponse } from "@/types/AdminManagement";
+
+/* 어드민 주문 상태 조회 */
+export async function fetchAdminOrderList(orderId?: number): Promise<AdminOrderResponse[]> {
+  try {
+    const response = await axiosInstance.get("/api/admin/orders", {
+      params: orderId ? { orderId: orderId } : {},
+    });
+    console.log(response.data.orders);
+
+    return response.data.orders;
+  } catch (error) {
+    console.error("주문 목록 불러오기 실패:", error);
+    throw error;
+  }
+}
+
+/* 어드민 주문 상태 변경 */
+export async function updateAdminOrderStatus(
+  orderId: number,
+  newStatus: AdminOrderResponse["orderStatus"]
+): Promise<void> {
+  try {
+    await axiosInstance.patch(`/api/admin/orders/${orderId}/status`, {
+      orderStatus: newStatus,
+    });
+  } catch (error) {
+    console.error(`주문 상태 변경 실패 (orderId: ${orderId})`, error);
+    throw error;
+  }
+}
