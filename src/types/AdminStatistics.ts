28ffe662827696e682c36b5702165a65331b296c
김현지 <chik1y@kookmin.ac.kr>
2025-06-18 09:27:03 +0900

    [FIX] 통계 응답 타입 지정 (#63)

--- src/types/AdminStatistics.ts
+++ src/types/AdminStatistics.ts
@@ -2,3 +2,21 @@ export interface DosirakSearch {
   dosirakId: number;
   name: string;
 }
+
+export interface SalesResponse{
+  day: string;
+  count: number;
+}
+
+export interface PopularResponse{
+  rank: number;
+  dosirakId: number;
+  name: string;
+  imageUrl: string;
+  count: number;
+}
+
+export interface OrderResponse{
+  single: number;
+  group: number;
+}
