67b5469963cc986d287cc55f55daa3ef8f91f931
김현지 <chik1y@kookmin.ac.kr>
2025-06-18 10:28:03 +0900

    [FEAT] 메인페이지 제로 웨이스트 API 연결 (#63)

--- src/components/main/RankingList.tsx
+++ src/components/main/RankingList.tsx
@@ -1,20 +1,29 @@
+import { useEffect, useState } from "react";
+import { fetchUniversityRanking } from "@/api/UniversityRanking";
+import { University } from "@/types/UniversityRanking";
 import CountUp from "react-countup";
 import styles from "@/css/main/RankingList.module.css";
 
 const RankingList = () => {
-  const rankings = [
-    { rank: 1, school: "연세대학교", count: 124 },
-    { rank: 2, school: "경희대학교", count: 98 },
-    { rank: 3, school: "이화여대", count: 91 },
-  ];
+  const [rankings, setRankings] = useState<University[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchUniversityRanking();
+      if (data) {
+        setRankings(data.slice(0, 3));
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className={styles.rankingWrapper}>
       <div className={styles.rankingLabel}>실시간 인증 랭킹</div>
       <ul className={styles.rankingList}>
-        {rankings.map((item) => (
-          <li key={item.rank} className={styles.rankingItem}>
-            {item.rank}위 {item.school} -{" "}
+        {rankings.map((item, index) => (
+          <li key={item.name} className={styles.rankingItem}>
+            {index + 1}위 {item.name} -{" "}
             <CountUp end={item.count} duration={1.5} enableScrollSpy />회
           </li>
         ))}
